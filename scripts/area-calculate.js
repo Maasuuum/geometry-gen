dece210e8bdb6bccb900e56be2b9115feb9be4e3
Maasuuum <fardousalmasum09@gmail.com>
2024-06-11 10:46:56 +0600

    input area base and height added

---
+++ scripts/area-calculate.js
@@ -0,0 +1,13 @@
+function calculateTriangleArea(){
+    // get triangle base value
+    const baseField = document.getElementById('triangle-base')
+    const baseValueText = baseField.value;
+    const base = parseFloat(baseValueText)
+    console.log(baseValueText);
+
+    // get triangle height value
+    const heightField = document.getElementById('triangle-height');
+    const heightValueText = heightField.value;
+    const height = parseFloat(heightValueText);
+    console.log(height);
+}
