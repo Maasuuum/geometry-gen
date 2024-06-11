5de902ec27d054752d6d932a61ff87660fd461bc
Maasuuum <fardousalmasum09@gmail.com>
2024-06-11 16:29:08 +0600

    parallelogram area calculated

--- scripts/area-calculate.js
+++ scripts/area-calculate.js
@@ -49,6 +49,7 @@ function calculateParallelogramArea(){
     console.log(height);
 
     const area = base * height;
+    setElementInnerText('parallelogram-area', area);
 
 }
 // reusable get input value field in number
@@ -57,4 +58,9 @@ function getInputValue(fieldId){
     const inputValueText = inputField.value;
     const value = parseFloat(inputValueText);
     return value;
+}
+// reusable set span, p, div, etc text
+function setElementInnerText(elementId, area){
+    const element = document.getElementById(elementId);
+    element.innerText = area;
 }
