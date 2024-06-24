60641fd9e41abf1554d3db61fa6f34b448f02baa
Maasuuum <fardousalmasum09@gmail.com>
2024-06-24 08:57:47 +0600

    ellipses area

--- scripts/area-calculate.js
+++ scripts/area-calculate.js
@@ -52,6 +52,14 @@ function calculateParallelogramArea(){
     setElementInnerText('parallelogram-area', area);
 
 }
+
+function calculateEllipseArea(){
+    const majorRadius = getInputValue('ellipse-major-radius');
+    const minorRadius = getInputValue('ellipse-minor-radius');
+    const area = 3.14 * majorRadius * minorRadius;
+    setElementInnerText('ellipse-area', area);
+}
+
 // reusable get input value field in number
 function getInputValue(fieldId){
     const inputField = document.getElementById(fieldId);
